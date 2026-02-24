829deece563a9234c8d47580b7aec525616a0c6d
Manuel Zorrilla <manuelzorrilladev@gmail.com>
2026-02-24 13:08:51 -0400

    feat (models): Added relationships to PublicationView

--- app/Models/PublicationView.php
+++ app/Models/PublicationView.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\Publication;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PublicationView extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $fillable = ['publication_id', 'ip', 'user_agent', 'viewer_id', 'viewed_at'];
+
+    public function publication(): BelongsTo
+    {
+        return $this->belongsTo(Publication::class);
+    }
+
+    public function viewer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'viewer_id');
+    }
 }
