13aa401bc2eead4bad60622b50d328a4fcee0513
Manuel Zorrilla <manuelzorrilladev@gmail.com>
2026-08-18 13:05:12 -0400

    feat(api): added support for password reset and custom mailing

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Publication;
 use App\Models\UserRecoveryCode;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
+use App\Notifications\CustomResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+
 
 class User extends Authenticatable
 {
@@ -61,6 +63,10 @@ class User extends Authenticatable
         ];
     }
 
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomResetPassword($token));
+    }
 
     public function publications()
     {
---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends Notification
+{
+    use Queueable;
+
+    public function __construct(public string $token) {}
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Restablece tu contraseña')
+            ->view('emails.custom-reset', [
+                'resetUrl' => $url,
+                'user'     => $notifiable,
+            ]);
+    }
+}
